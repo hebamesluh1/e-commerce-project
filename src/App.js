958ab333b58febb1a47a423f3e7156c80e829e11
hebamesluh1 <hebamesluh@gmail.com>
2023-02-04 18:37:10 +0200

    add structure of project

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useRoutes } from "react-router-dom";
+import React, { Suspense } from 'react';
+import { ThemeProvider } from "styled-components";
+import { router as routes } from './routes';
+
+//Style
+import { GlobalStyle } from "./Global/style";
+import { lightTheme} from "./Global/theme";
+
+//context
+import { themeContext } from "./Context/themeContext";
+import { useEffect, useState } from "react";
+import { AuthContext } from './Context/authContext';
+
+function App() {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [theme, setTheme] = useState(lightTheme);
+
+  useEffect(() => {
+    localStorage.getItem("Theme") === "dark" ? setTheme(lightTheme) : setTheme(lightTheme);
+  
+  }, []); 
+  const router = useRoutes(routes);
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthContext.Provider value={[isAuthorized, setIsAuthorized]}>
+        <themeContext.Provider value={[theme, setTheme]}>
+            <GlobalStyle />
+            <Suspense fallback={<div className="lds-dual-ring"></div>}>{router}</Suspense>
+        </themeContext.Provider>
+      </AuthContext.Provider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
