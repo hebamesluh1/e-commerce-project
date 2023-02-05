ac307a56388e6845b4f461dae872395bb6c5f75b
hebamesluh1 <hebamesluh@gmail.com>
2023-02-05 19:21:06 +0200

    ui code to page not found

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NotFoundMessage from '../Components/NotFoundMessage'
+
+function NotFound() {
+    return (
+        <>
+            <NotFoundMessage />
+        </>
+    )
+}
+
+export default NotFound
