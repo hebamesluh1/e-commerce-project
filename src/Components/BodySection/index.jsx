306f7b8f2fcddd0e1647ba3ec4df81604d20d8a2
hebamesluh1 <hebamesluh@gmail.com>
2023-02-06 11:16:41 +0200

    update

--- src/Components/BodySection/index.jsx
+++ src/Components/BodySection/index.jsx
@@ -14,8 +14,7 @@ import pic6 from '../../Assets/image/ListStore/components/10.png';
 export default function BodySection() {
     return (
         <div>
-            <Card img={pic6} title="Canon Cmera EOS 2000, Black 10x zoom" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua" discount="$1128.00"/>
+            <Card img={pic6} title="Canon Cmera EOS 2000, Black 10x zoom" description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" discount="$1128.00"/>
             <Card img={pic5} title="GoPro HERO6 4K Action Camera - Black" description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit "/>
             <Card img={pic4} title="GoPro HERO6 4K Action Camera - Black" description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit "/>
             <Card img={pic3} title="GoPro HERO6 4K Action Camera - Black" description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit "/>
