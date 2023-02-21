f8454b11a66049f7edc0f71e2e43eed987f6b336
hebamesluh1 <hebamesluh@gmail.com>
2023-02-21 18:30:41 +0200

    add susponse inside second section

--- src/Pages/TechStore/sections/SecondSection/index.jsx
+++ src/Pages/TechStore/sections/SecondSection/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{Suspense} from 'react'
 import styled from 'styled-components'
 import { Outlet } from 'react-router-dom'
 import HeaderSection from './components/HeaderSection'
@@ -10,7 +10,9 @@ export default function SecondSection() {
     return (
         <Style>
             <HeaderSection />
+            <Suspense fallback={<div className="lds-dual-ring"></div>}>
             <Outlet />
+          </Suspense>
         </Style>
     )
 }
